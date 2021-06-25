6c2cf9ad53624d8a9d61360a50501aa4f245e7c5
Oliver-Sandic <85484466+Oliver-Sandic@users.noreply.github.com>
2021-06-26 01:31:23 +0200

    Add files via upload
    UPDATE: Spielprinzip über Server fast fertig; nur noch troubleshooting

--- Mensch-aergere-dich-nicht.py
+++ Mensch-aergere-dich-nicht.py
@@ -22,10 +22,11 @@ send_disable = False
 
 class canvas(Frame):
     def __init__(self, master=None):
-        global count
+        global count, your_turn
         super().__init__(master)
         self.coords = []
         self.master = master
+        self.receive()
         self.newWindow = Toplevel(self.master)
         self.newWindow.after(1, lambda: self.newWindow.focus_force())
         Dice(self.newWindow)
@@ -35,21 +36,21 @@ class canvas(Frame):
     
     def new_window(self, event):
         global dice_allowed, dice_force, count, your_turn, send_disable, recevie_disable
+        
         if your_turn == False:
+            self.receive()
             return
-        
-        if send_disable == False:
-            send_thread = threading.Thread(target=self.send)
-            send_thread.daemon = True
-            send_thread.start()
-            
-        if recevie_disable == False:
-            receive_thread = threading.Thread(target=self.receive)
-            receive_thread.start()
-        
+
         self.caller = event.widget.find_withtag("current")[0]
         if dice_force == True:
             Dice(self.newWindow)
+        
+        if count == False:
+            your_turn = False
+            dice_force = False
+            dice_allowed = False
+            self.send()
+            return
             
         if dice_allowed==False:
             y=list(self.coords[self.caller-1])
@@ -71,9 +72,6 @@ class canvas(Frame):
                     self.main_board.itemconfigure(self.caller, fill = "white")
                     return
         y=list(self.coords[self.caller-1])
-        print(self.caller)
-        print(y[4])
-        print(num)
         for j in range (0,5,1):
             for i in range (1+j,7,1):
                 if ((self.caller == 22-j) and (y[4] == "red") and (num >= j and num <= 4+j)):
@@ -136,6 +134,7 @@ class canvas(Frame):
             
         self.main_board.itemconfigure(self.caller+num,fill="blue")
         print("you clicked:"+str(self.caller))
+        self.send()
         dice_allowed = True
 
     def create_board(self):
@@ -148,8 +147,6 @@ class canvas(Frame):
         self.main_board = Canvas(self,width=800, height=700, bg = "#F5F6CE")
         self.main_board.pack()
         
-        #for i in range(0,2,1):
-        #    self.coords.append((350+(50*i),75,385+(50*i),110,"#FFFFFF"))
         self.coords.append((350,75,385,110,"#FFFFFF"))
         self.coords.append((400,75,435,110,"#FFFFFF"))
         self.coords.append((450,75,485,110,"#819FF7"))
@@ -217,67 +214,140 @@ class canvas(Frame):
         self.main_board.create_oval(600,560,635,525,fill="#F2F5A9")
         
     def send(self):
-        global recevie_disable
-        recevie_disable = True
+        global your_turn, recevie_disable
+        #recevie_disable = True
+        your_turn = False
+        s.sendall(pickle.dumps(self.coords))
         print("sent")
+        received = s.recv(1024)
+        received_readable = pickle.loads(received)
+        self.coords = received_readable
+        print("received=", received_readable)
+        received = s.recv(1024)
+        received_readable = pickle.loads(received)
+        self.coords = received_readable
+        print("received", received_readable)
+        your_turn=True
+        print(received_readable)
         
     def receive(self):
-        global send_disable
-        send_disable = True
-        print("received")
+        global your_turn, send_disable
+        #send_disable = True
+        received = s.recv(1024)
+        received_readable = pickle.loads(received)
+        
+        print(received_readable)
+        
+        if type(received_readable) == str:
+            if received_readable == "TURN":
+                print("SUBBA")
+                your_turn = True
+            else:
+                your_turn = False
+        else:
+            your_turn = True
+            self.coords = received_readable
+            print("received")
+            print(received_readable)
             
 class Dice:
     def __init__(self, master):
         self.master = master
         master.title("Würfel")
-        master.geometry("200x200")
+        master.geometry("200x180")
         self.play()
         
     def play(self):
+        global dice_allowed
         self.frame = Frame(self.master)
         self.frame.pack()
         self.dice = Canvas(self.frame ,width=150, height=150, bg = "#F5F6CE")
         self.dice.pack()
-        self.dice_allowed = True
+        dice_allowed = True
         def roll_dice(event):
             global num, dice_allowed, dice_force, count
             while (dice_allowed == True):
-                self.dice.delete("all")
-                self.num = random.randint(1,6)
+                if num == 0:
+                    for i in range (0,3,1):
+                        self.dice.delete("all")
+                        self.num = random.randint(1,6)
+                        print(self.num)
+                        print(i)
+                        if self.num==1:
+                            oval = self.dice.create_oval(70,70,85,85, fill="black")
+                        elif self.num==2:
+                            oval = self.dice.create_oval(105,35,120,50, fill="black")
+                            oval = self.dice.create_oval(50,105,35,120, fill="black")
+                        elif self.num==3:
+                            oval = self.dice.create_oval(105,35,120,50, fill="black")
+                            oval = self.dice.create_oval(50,105,35,120, fill="black")
+                            oval = self.dice.create_oval(70,70,85,85, fill="black")
+                        elif self.num==4:
+                            oval = self.dice.create_oval(105,35,120,50, fill="black")
+                            oval = self.dice.create_oval(50,105,35,120, fill="black")
+                            oval = self.dice.create_oval(105,105,120,120, fill="black")
+                            oval = self.dice.create_oval(50,35,35,50, fill="black")
+                        elif self.num==5:
+                            oval = self.dice.create_oval(105,35,120,50, fill="black")
+                            oval = self.dice.create_oval(50,105,35,120, fill="black")
+                            oval = self.dice.create_oval(105,105,120,120, fill="black")
+                            oval = self.dice.create_oval(50,35,35,50, fill="black")
+                            oval = self.dice.create_oval(70,70,85,85, fill="black")
+                        else:
+                            oval = self.dice.create_oval(105,25,120,40, fill="black")
+                            oval = self.dice.create_oval(50,115,35,130, fill="black")
+                            oval = self.dice.create_oval(105,115,120,130, fill="black")
+                            oval = self.dice.create_oval(50,25,35,40, fill="black")
+                            oval = self.dice.create_oval(50,70,35,85, fill="black")
+                            oval = self.dice.create_oval(105,70,120,85, fill="black")
+                        if self.num == 6:
+                            num = self.num
+                            dice_force = False
+                            print(num)
+                            return
+                        else:
+                            if i == 2:
+                                count = False
+                                dice_allowed = False
+                                return
+                            continue
                 
-                if self.num==1:
-                    oval = self.dice.create_oval(70,70,85,85, fill="black")
-                elif self.num==2:
-                    oval = self.dice.create_oval(105,35,120,50, fill="black")
-                    oval = self.dice.create_oval(50,105,35,120, fill="black")
-                elif self.num==3:
-                    oval = self.dice.create_oval(105,35,120,50, fill="black")
-                    oval = self.dice.create_oval(50,105,35,120, fill="black")
-                    oval = self.dice.create_oval(70,70,85,85, fill="black")
-                elif self.num==4:
-                    oval = self.dice.create_oval(105,35,120,50, fill="black")
-                    oval = self.dice.create_oval(50,105,35,120, fill="black")
-                    oval = self.dice.create_oval(105,105,120,120, fill="black")
-                    oval = self.dice.create_oval(50,35,35,50, fill="black")
-                elif self.num==5:
-                    oval = self.dice.create_oval(105,35,120,50, fill="black")
-                    oval = self.dice.create_oval(50,105,35,120, fill="black")
-                    oval = self.dice.create_oval(105,105,120,120, fill="black")
-                    oval = self.dice.create_oval(50,35,35,50, fill="black")
-                    oval = self.dice.create_oval(70,70,85,85, fill="black")
                 else:
-                    count = False
-                    oval = self.dice.create_oval(105,25,120,40, fill="black")
-                    oval = self.dice.create_oval(50,115,35,130, fill="black")
-                    oval = self.dice.create_oval(105,115,120,130, fill="black")
-                    oval = self.dice.create_oval(50,25,35,40, fill="black")
-                    oval = self.dice.create_oval(50,70,35,85, fill="black")
-                    oval = self.dice.create_oval(105,70,120,85, fill="black")
-                
-                num = self.num
-                dice_force = False
-                print(num)
-                dice_allowed = False
+                    self.dice.delete("all")
+                    self.num = random.randint(1,6)
+                    
+                    if self.num==1:
+                        oval = self.dice.create_oval(70,70,85,85, fill="black")
+                    elif self.num==2:
+                        oval = self.dice.create_oval(105,35,120,50, fill="black")
+                        oval = self.dice.create_oval(50,105,35,120, fill="black")
+                    elif self.num==3:
+                        oval = self.dice.create_oval(105,35,120,50, fill="black")
+                        oval = self.dice.create_oval(50,105,35,120, fill="black")
+                        oval = self.dice.create_oval(70,70,85,85, fill="black")
+                    elif self.num==4:
+                        oval = self.dice.create_oval(105,35,120,50, fill="black")
+                        oval = self.dice.create_oval(50,105,35,120, fill="black")
+                        oval = self.dice.create_oval(105,105,120,120, fill="black")
+                        oval = self.dice.create_oval(50,35,35,50, fill="black")
+                    elif self.num==5:
+                        oval = self.dice.create_oval(105,35,120,50, fill="black")
+                        oval = self.dice.create_oval(50,105,35,120, fill="black")
+                        oval = self.dice.create_oval(105,105,120,120, fill="black")
+                        oval = self.dice.create_oval(50,35,35,50, fill="black")
+                        oval = self.dice.create_oval(70,70,85,85, fill="black")
+                    else:
+                        oval = self.dice.create_oval(105,25,120,40, fill="black")
+                        oval = self.dice.create_oval(50,115,35,130, fill="black")
+                        oval = self.dice.create_oval(105,115,120,130, fill="black")
+                        oval = self.dice.create_oval(50,25,35,40, fill="black")
+                        oval = self.dice.create_oval(50,70,35,85, fill="black")
+                        oval = self.dice.create_oval(105,70,120,85, fill="black")
+                    
+                    num = self.num
+                    dice_force = False
+                    print(num)
+                    dice_allowed = False
         
         self.diceButton = Button(self.frame, text = 'würfeln', width = 5)
         self.diceButton.pack(side = "bottom")
@@ -288,6 +358,9 @@ PORT = 61111
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+data = "NEW"
+s.sendall(pickle.dumps(data))
 
 root_window = Tk()
 app = canvas(root_window)
